898c19899e93419fa24f109a21cf0d5b3326b16b
Alex Blass <ajblack.000@hotmail.com>
2017-09-28 10:43:27 -0700

    Feat: Save recipes to SQLite db for offline view

--- app/src/main/java/net/alexblass/bakingapp/MainActivityFragment.java
+++ app/src/main/java/net/alexblass/bakingapp/MainActivityFragment.java
@@ -2,11 +2,14 @@ package net.alexblass.bakingapp;
 
 import android.app.Fragment;
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import net.alexblass.bakingapp.data.RecipesContract.RecipeEntry;
 import net.alexblass.bakingapp.models.Recipe;
 import net.alexblass.bakingapp.utilities.RecipeAdapter;
 import net.alexblass.bakingapp.utilities.RecipeLoader;
@@ -32,6 +36,11 @@ public class MainActivityFragment extends Fragment
 
     // The ID for the recipe loader
     private static final int RECIPE_LOADER_ID = 0;
+    // The ID for the recipe favorites loader
+    private static final int FAVORITE_RECIPE_LOADER_ID = 1;
+
+    // The query URL
+    private String mUrl;
 
     // Displays a message when there is no Internet or when there are no Recipes found
     private TextView mErrorMessageTextView;
@@ -61,6 +70,8 @@ public class MainActivityFragment extends Fragment
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        mUrl = getActivity().getResources().getString(R.string.query_url);
+
         // Find the RecyclerView and set our adapter to it so the recipes
         // display in a vertical linear layout format.
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recipes_rv);
@@ -87,15 +98,13 @@ public class MainActivityFragment extends Fragment
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
 
-        // If there is no connectivity, show an error message
+        // If there is no connectivity, use our local SQLite database to show recipes
         if (isConnected) {
-            showRecyclerView();
             loaderManager.initLoader(RECIPE_LOADER_ID, null, this);
         } else {
-            mLoadingIndicator.setVisibility(View.GONE);
-            showErrorMessage();
-            mErrorMessageTextView.setText(R.string.no_connection);
+            loaderManager.initLoader(FAVORITE_RECIPE_LOADER_ID, null, this);
         }
+        showRecyclerView();
 
         return rootView;
     }
@@ -112,16 +121,72 @@ public class MainActivityFragment extends Fragment
         mErrorMessageTextView.setVisibility(View.VISIBLE);
     }
 
+    // Add a recipe to the database
+    private void addRecipe(Recipe recipe, boolean isFave){
+        ContentValues values = new ContentValues();
+        values.put(RecipeEntry.COLUMN_RECIPE_SOURCE_ID, recipe.getId());
+        values.put(RecipeEntry.COLUMN_NAME, recipe.getName());
+        values.put(RecipeEntry.COLUMN_SERVINGS, recipe.getServings());
+        values.put(RecipeEntry.COLUMN_IMG_URL, recipe.getImageUrl());
+        values.put(RecipeEntry.COLUMN_IS_FAVORITED, isFave);
+
+        Uri newUri = getActivity().getContentResolver().insert(RecipeEntry.CONTENT_URI, values);
+    }
+
+    // Updates the table so that we have the most recent recipe data without adding duplicates
+    private void updateTable(Recipe recipe, boolean isFave){
+        // Get the Recipe by its source id since any user-added recipes will not have one
+        String sourceId = Integer.toString(recipe.getId());
+
+        String[] projection = {
+                RecipeEntry.COLUMN_RECIPE_SOURCE_ID
+        };
+
+        String selection = RecipeEntry.COLUMN_RECIPE_SOURCE_ID + "=?";
+
+        String[] selectionArgs = {sourceId};
+
+        Cursor cursor = getActivity().getContentResolver().query(
+                RecipeEntry.CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null
+        );
+
+        // Only add the recipe to the database if it does not already exist
+        if (!cursor.moveToFirst()){
+            addRecipe(recipe, isFave);
+        }
+
+        cursor.close();
+    }
+
     // Load the data from the JSON file URL
     @Override
     public Loader<Recipe[]> onCreateLoader(int id, Bundle args) {
         RecipeLoader recipeLoader;
-        if (id == RECIPE_LOADER_ID){
-            mErrorMessage = getString(R.string.no_results);
-            recipeLoader = new RecipeLoader(getActivity());
-            return recipeLoader;
+        mErrorMessage = getString(R.string.no_results);
+        switch(id) {
+            case RECIPE_LOADER_ID:
+                Uri baseUri = Uri.parse(mUrl);
+                Uri.Builder uriBuilder = baseUri.buildUpon();
+                recipeLoader = new RecipeLoader(getActivity(), uriBuilder.toString());
+                return recipeLoader;
+            case FAVORITE_RECIPE_LOADER_ID:
+                // Since we're pulling our data from the database, we do not need a URL
+                recipeLoader = new RecipeLoader(getActivity(), null);
+                // If there are no favorites, show an error message
+                if(recipeLoader.loadInBackground() == null){
+                    mLoadingIndicator.setVisibility(View.GONE);
+                    mErrorMessage = getString(R.string.no_results);
+                    showErrorMessage();
+                }
+                return recipeLoader;
+            default:
+                return null;
         }
-        return null;
     }
 
     // When the Loader finishes loading, add the list of Recipes to the Adapter data set
@@ -133,6 +198,10 @@ public class MainActivityFragment extends Fragment
 
         if (newRecipes != null && newRecipes.length > 0){
             mAdapter.setAllRecipies(newRecipes);
+
+            for (Recipe r : newRecipes) {
+                updateTable(r, false);
+            }
         } else {
             showErrorMessage();
         }
--- app/src/main/java/net/alexblass/bakingapp/utilities/RecipeLoader.java
+++ app/src/main/java/net/alexblass/bakingapp/utilities/RecipeLoader.java
@@ -2,9 +2,11 @@ package net.alexblass.bakingapp.utilities;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.database.Cursor;
 
-import net.alexblass.bakingapp.R;
+import net.alexblass.bakingapp.data.RecipesContract.RecipeEntry;
 import net.alexblass.bakingapp.models.Recipe;
+
 
 /**
  * Loads a list of Recipes using the AsyncTask
@@ -13,11 +15,12 @@ import net.alexblass.bakingapp.models.Recipe;
 
 public class RecipeLoader extends AsyncTaskLoader<Recipe[]> {
     // The query URL
-    private String mUrl = getContext().getResources().getString(R.string.query_url);
+    private String mUrl = null;
 
     // Constructs a new RecipeLoader
-    public RecipeLoader(Context context){
+    public RecipeLoader(Context context, String url){
         super(context);
+        mUrl = url;
     }
 
     @Override
@@ -27,6 +30,65 @@ public class RecipeLoader extends AsyncTaskLoader<Recipe[]> {
 
     @Override
     public Recipe[] loadInBackground() {
+        Recipe[] recipes = null;
+
+        if (mUrl == null){
+
+            String[] projection = {
+                    RecipeEntry._ID,
+                    RecipeEntry.COLUMN_RECIPE_SOURCE_ID,
+                    RecipeEntry.COLUMN_NAME,
+                    RecipeEntry.COLUMN_SERVINGS,
+                    RecipeEntry.COLUMN_IMG_URL,
+                    RecipeEntry.COLUMN_IS_FAVORITED
+            };
+
+            // Check if the recipe exists in the database
+            Cursor cursor = getContext().getContentResolver().query(
+                    RecipeEntry.CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+
+            if (cursor.moveToFirst()){
+                recipes = new Recipe[cursor.getCount()];
+
+                String name, img_url;
+                int sourceId, servings;
+                boolean isFavorite;
+
+                sourceId = cursor.getInt(cursor.getColumnIndex(RecipeEntry.COLUMN_RECIPE_SOURCE_ID));
+                name = cursor.getString(cursor.getColumnIndex(RecipeEntry.COLUMN_NAME));
+                servings = cursor.getInt(cursor.getColumnIndex(RecipeEntry.COLUMN_SERVINGS));
+                img_url = cursor.getString(cursor.getColumnIndex(RecipeEntry.COLUMN_IMG_URL));
+
+                Recipe recipe = new Recipe(
+                        sourceId, name, null, null, servings, img_url
+                        );
+                recipes[0] = recipe;
+
+                for (int i = 1; cursor.moveToNext(); i++) {
+
+                    sourceId = cursor.getInt(cursor.getColumnIndex(RecipeEntry.COLUMN_RECIPE_SOURCE_ID));
+                    name = cursor.getString(cursor.getColumnIndex(RecipeEntry.COLUMN_NAME));
+                    servings = cursor.getInt(cursor.getColumnIndex(RecipeEntry.COLUMN_SERVINGS));
+                    img_url = cursor.getString(cursor.getColumnIndex(RecipeEntry.COLUMN_IMG_URL));
+                    //isFavorite = (cursor.getInt(cursor.getColumnIndex(RecipeEntry.COLUMN_IS_FAVORITED)) == 1);
+
+                    recipe = new Recipe(
+                            sourceId, name, null, null, servings, img_url
+                    );
+                    recipes[i] = recipe;
+                }
+            }
+
+            cursor.close();
+            return recipes;
+        }
+
         return QueryUtils.fetchRecipes(mUrl);
     }
 }
