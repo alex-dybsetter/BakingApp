75898c2ba107bcb1857281780911072e9da3e9a6
Alex Blass <ajblack.000@hotmail.com>
2017-10-10 06:53:24 -0700

    Feat: Add layout for new add recipe activity

--- app/src/main/java/net/alexblass/bakingapp/MainActivity.java
+++ app/src/main/java/net/alexblass/bakingapp/MainActivity.java
@@ -1,12 +1,14 @@
 package net.alexblass.bakingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Implement sharing functionality
-    // TODO: Implement user adds recipe functionality
     // TODO: Verify localization requirements
     // TODO: Verify accessibility requirements
     // TODO: Add butterknife
@@ -19,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Inflate our options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Determine what action to take based on the menu item selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_add_recipe){
+            Intent addActivityIntent = new Intent(MainActivity.this, AddRecipeActivity.class);
+            startActivity(addActivityIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
